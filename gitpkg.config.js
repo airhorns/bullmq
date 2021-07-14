c718540d6bb51d4e5def34131041bbc2a12fb554
Harry Brundage <harry.brundage@gmail.com>
2021-07-14 08:29:22 -0400

    gitpkg

---
+++ gitpkg.config.js
@@ -0,0 +1,9 @@
+const { execSync } = require('child_process')
+
+module.exports = () => ({
+  getTagName: (pkg) =>
+    `${pkg.name}-v${pkg.version}-gitpkg-2-${execSync(
+      'git rev-parse --short HEAD',
+      { encoding: 'utf-8' }
+    ).trim()}`
+})
---
+++ src/interfaces/queue-options.ts
@@ -0,0 +1,40 @@
+import { JobsOptions } from '../interfaces';
+
+import { Redis } from 'ioredis';
+import { ConnectionOptions } from './redis-options';
+
+export enum ClientType {
+  blocking = 'blocking',
+  normal = 'normal',
+}
+
+export interface QueueBaseOptions {
+  connection?: ConnectionOptions;
+  client?: Redis;
+  /**
+   * Prefix for all queue keys.
+   */
+  prefix?: string;
+}
+
+export interface QueueOptions extends QueueBaseOptions {
+  defaultJobOptions?: JobsOptions;
+
+  limiter?: {
+    groupKey: string;
+  };
+
+  streams?: {
+    events: {
+      /**
+       * Max aproximated length for streams
+       */
+      maxLen: number;
+    };
+  };
+}
+
+export interface QueueEventsOptions extends QueueBaseOptions {
+  lastEventId?: string;
+  blockingTimeout?: number;
+}
